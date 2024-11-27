37f4a17a70d3f2540af89186fce7e52ba97ca950
SaidaCabezas <cabezassaida2003@gmail.com>
2024-11-27 09:24:50 -0500

    Create hijos.java

---
+++ hijos.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Hp
+ */
+public class hijos  {
+    
+    String nombre ;
+    String apellido;
+    int edad;
+    double peso;
+    double tamaño;
+    
+    public hijos(){
+        
+        }
+
+    public hijos(String nombre, String apellido, int edad, double peso, double tamaño) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.peso = peso;
+        this.tamaño = tamaño;
+    }
+
+
+    }
